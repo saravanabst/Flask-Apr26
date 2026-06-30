dad106b6b788c7196559245084860f0b4144406f
Saravanakumur Kannan <saravanabst@gmail.com>
2026-06-30 20:38:44 +0530

    update CD sequencial

--- loan.py
+++ loan.py
@@ -9,8 +9,8 @@ with open('classifier.pkl', 'rb') as f:
 
 @app.route('/', methods = ['GET'])
 def home(): 
-    return "Welcome to the Loan Management System!"
-
+    return "Welcome to the Loan Management System!!!"
+     
 @app.route('/predict', methods = ['GET'])
 def predict(): 
     return "I predict your loan approval status."
